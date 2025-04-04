a1fbb792ef7110402001f7c3ff4f42d4b79dd3ff
Manthan Bhatt <bhattmanthank@gmail.com>
2025-04-04 07:16:24 +0530

    Update main.cpp

--- Longest-Consecutive-Sequence/main.cpp
+++ Longest-Consecutive-Sequence/main.cpp
@@ -1,52 +1,51 @@
 #include <iostream>
 #include <vector>
-#include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution
 {
-  public:
-  int longcons(vector<int>&num)
-  {
-      if(num.empty())
-      {
-          return 0;
-      }
-      unordered_set<int> set(num.begin(),num.end());
-      int streak=0;
-      for(int n:set)
-      {
-          if(set.find(n-1)==set.end())
-          {
-              int curr=n;
-              int currstreak=1;
-              while(set.find(curr+1)!=set.end())
-              {
-                  curr++;
-                  currstreak++;
-              }
-              streak=max(streak,currstreak);
-          }
-      }
-      return streak;
-  }
+public:
+    int longcons(vector<int>& num)
+    {
+        unordered_map<int, int> map;
+        int maxLen = 0;
+
+        for (int n : num)
+        {
+            if (map.find(n) == map.end())
+            {
+                int left = map.find(n - 1) != map.end() ? map[n - 1] : 0;
+                int right = map.find(n + 1) != map.end() ? map[n + 1] : 0;
+                int length = left + right + 1;
+
+                map[n] = length;
+                maxLen = max(maxLen, length);
+
+                map[n - left] = length;
+                map[n + right] = length;
+            }
+        }
+
+        return maxLen;
+    }
 };
 
 int main()
 {
     vector<int> num;
-    int n,ele;
-    cout<<"size";
-    cin>>n;
-    cout<<"element";
-    for(int i=0;i<n;i++)
+    int n, ele;
+    cout << "size: ";
+    cin >> n;
+    cout << "elements: ";
+    for (int i = 0; i < n; i++)
     {
-        cin>>ele;
+        cin >> ele;
         num.push_back(ele);
     }
     Solution sol;
-    cout<<sol.longcons(num)<<endl;
+    cout << "Longest consecutive sequence length: " << sol.longcons(num) << endl;
 
     return 0;
 }
