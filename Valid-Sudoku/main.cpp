ce7e52f9b650015e572085a3c9a3693ff780bf02
Manthan Bhatt <bhattmanthank@gmail.com>
2025-04-04 07:36:03 +0530

    Update main.cpp

--- Valid-Sudoku/main.cpp
+++ Valid-Sudoku/main.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <unordered_map>
 #include <vector>
@@ -5,43 +6,43 @@ using namespace std;
 
 class Solution
 {
-  public:
-  bool isValid(vector<vector<char>>&board)
-  {
-      unordered_map<int,unordered_map<char,int>> row,col,box;
-      for(int i=0;i<9;i++)
-      {
-          for(int j=0;j<9;j++)
-          {
-              char num=board[i][j];
-              if(num=='.')
-              {
-                  continue;
-              }
-              int bindex=(i/3)*3+(j/3);
-              if(row[i][num]++ > 0|| col[j][num]++>0||box[bindex][num]++>0)
-              {
-                  return false;
-              }
-          }
-      }
-    return true;
-  }
+public:
+    bool isValid(vector<vector<char>> &board)
+    {
+        unordered_map<int, unordered_map<char, int>> row, col, box;
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                char num = board[i][j];
+                if (num == '.')
+                {
+                    continue;
+                }
+                int bindex = (i / 3) * 3 + (j / 3);
+                if (row[i][num]++ > 0 || col[j][num]++ > 0 || box[bindex][num]++ > 0)
+                {
+                    return false;
+                }
+            }
+        }
+        return true; // Move return true here to check all rows, columns, and boxes first
+    }
 };
 
 int main()
 {
-    vector<vector<char>> board(9,vector<char>(9));
-    cout<<"use . for empty cells";
-    for(int i=0;i<9;i++)
+    vector<vector<char>> board(9, vector<char>(9));  // Fixed syntax error
+    cout << "Use '.' for empty cells:\n";
+    for (int i = 0; i < 9; i++)
     {
-        for(int j=0;j<9;j++)
+        for (int j = 0; j < 9; j++)
         {
-            cin>>board[i][j];
+            cin >> board[i][j];
         }
     }
     Solution sol;
-    cout<<(sol.isValid(board)?"Valid":"Invalid");
+    cout << (sol.isValid(board) ? "Valid" : "Invalid") << endl;
 
     return 0;
 }
